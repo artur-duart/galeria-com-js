199567b0964348857e1b4627a900af7764a7b526
Artur <arturduartemoraes@gmail.com>
2021-09-29 11:05:09 -0300

    Função de Slides feita no JavaScript

--- galeria.js
+++ galeria.js
@@ -12,14 +12,18 @@ const imagens = [
     "./img/thor2.jpg",
     "./img/captainAmerica2.jpg",
     "./img/guardiansGalaxy.jpg",
-    "./img/guardiansGalaxy2.jpg"
+    "./img/guardiansGalaxy2.jpg",
+    "./img/avengers2.jpg",
+    "./img/antMan.jpg"
 ]
+
+const limparId = (url) => url.replace("./img/", "").split(".")[0].replace(" ", "-")
 
 const criarItem = (urlImagem) => {
     const container = document.querySelector(".galeria-container")
 
     const novoLink = document.createElement("a")
-    novoLink.href = urlImagem
+    novoLink.href = `#${limparId(urlImagem)}`
     novoLink.classList.add("galeria-items")
     novoLink.innerHTML = `<img src="${urlImagem}" alt=""></img>`
 
@@ -35,6 +39,34 @@ const criarItem = (urlImagem) => {
 //     `
 // }
 
+const carregarGaleria = (imagens) => imagens.forEach(criarItem)
+
+const criarSlide = (urlImagem, indice, arr) => {
+    const container = document.querySelector(".slide-container")
+    const novoDiv = document.createElement("div")
+    novoDiv.classList.add("slide")
+    novoDiv.id = limparId(urlImagem)
+
+    const indiceAnterior = indice <= 0 ? arr.length - 1 : indice - 1
+    const iDAnterior = limparId(arr[indiceAnterior])
+
+    const indicePosterior = indice >= arr.length - 1 ? 0 : indice + 1
+    const iDPosterior = limparId(arr[indicePosterior])
+
+    novoDiv.innerHTML = `
+            <div class="imagem-container">
+                <a href="" class="icones fechar">&#128473;</a>
+                <a href="#${iDAnterior}" class="icones anterior">&#129168;</a>
+                <img src="${urlImagem}" alt="">
+                <a href="#${iDPosterior}" class="icones proximo">&#129170;</a>
+            </div>`
+
+    container.appendChild(novoDiv)
+}
+
+const carregarSlide = (imagens) => imagens.forEach(criarSlide)
+
 const carregarImagens = () => imagens.forEach(criarItem)
 
-carregarImagens()
+carregarImagens(imagens)
+carregarSlide(imagens)
