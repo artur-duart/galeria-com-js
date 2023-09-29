25e9a7bdb03e44a3684fc855eceb7e9d28fbc8af
Artur Duarte <arturduartemoraes@gmail.com>
2023-09-29 20:36:25 -0300

    👌 style: formatting project files

--- galeria.js
+++ galeria.js
@@ -1,40 +1,40 @@
-"use strict"
+'use strict';
 
 const imagens = {
-    "url": [
-        "./img/captainAmerica.jpg",
-        "./img/captainMarvel.jpg",
-        "./img/ironMan.jpg",
-        "./img/hulk.jpg",
-        "./img/ironMan2.jpg",
-        "./img/thor.jpg",
-        "./img/avengers.jpg",
-        "./img/ironMan3.jpg",
-        "./img/thor2.jpg",
-        "./img/captainAmerica2.jpg",
-        "./img/guardiansGalaxy.jpg",
-        "./img/guardiansGalaxy2.jpg",
-        "./img/avengers2.jpg",
-        "./img/antMan.jpg"
-    ]
-}
+	url: [
+		'./img/captainAmerica.jpg',
+		'./img/captainMarvel.jpg',
+		'./img/ironMan.jpg',
+		'./img/hulk.jpg',
+		'./img/ironMan2.jpg',
+		'./img/thor.jpg',
+		'./img/avengers.jpg',
+		'./img/ironMan3.jpg',
+		'./img/thor2.jpg',
+		'./img/captainAmerica2.jpg',
+		'./img/guardiansGalaxy.jpg',
+		'./img/guardiansGalaxy2.jpg',
+		'./img/avengers2.jpg',
+		'./img/antMan.jpg',
+	],
+};
 
 const limparId = (url) => {
-    const ultimaBarra = url.lastIndexOf("/") + 1
-    const ultimoPonto = url.lastIndexOf(".")
-    return url.substring(ultimaBarra, ultimoPonto).replace(" ", "-")
-}
+	const ultimaBarra = url.lastIndexOf('/') + 1;
+	const ultimoPonto = url.lastIndexOf('.');
+	return url.substring(ultimaBarra, ultimoPonto).replace(' ', '-');
+};
 
 const criarItem = (urlImagem) => {
-    const container = document.querySelector(".galeria-container")
+	const container = document.querySelector('.galeria-container');
 
-    const novoLink = document.createElement("a")
-    novoLink.href = `#${limparId(urlImagem)}`
-    novoLink.classList.add("galeria-items")
-    novoLink.innerHTML = `<img src="${urlImagem}" alt=""></img>`
+	const novoLink = document.createElement('a');
+	novoLink.href = `#${limparId(urlImagem)}`;
+	novoLink.classList.add('galeria-items');
+	novoLink.innerHTML = `<img src="${urlImagem}" alt=""></img>`;
 
-    container.appendChild(novoLink)
-}
+	container.appendChild(novoLink);
+};
 
 // const criarItem = (urlImagem) => {
 //     const container = document.querySelector(".galeria-container")
@@ -45,34 +45,34 @@ const criarItem = (urlImagem) => {
 //     `
 // }
 
-const carregarGaleria = (imagens) => imagens.forEach(criarItem)
+const carregarGaleria = (imagens) => imagens.forEach(criarItem);
 
 const criarSlide = (urlImagem, indice, arr) => {
-    const container = document.querySelector(".slide-container")
-    const novoDiv = document.createElement("div")
-    novoDiv.classList.add("slide")
-    novoDiv.id = limparId(urlImagem)
+	const container = document.querySelector('.slide-container');
+	const novoDiv = document.createElement('div');
+	novoDiv.classList.add('slide');
+	novoDiv.id = limparId(urlImagem);
 
-    const indiceAnterior = indice <= 0 ? arr.length - 1 : indice - 1
-    const iDAnterior = limparId(arr[indiceAnterior])
+	const indiceAnterior = indice <= 0 ? arr.length - 1 : indice - 1;
+	const iDAnterior = limparId(arr[indiceAnterior]);
 
-    const indicePosterior = indice >= arr.length - 1 ? 0 : indice + 1
-    const iDPosterior = limparId(arr[indicePosterior])
+	const indicePosterior = indice >= arr.length - 1 ? 0 : indice + 1;
+	const iDPosterior = limparId(arr[indicePosterior]);
 
-    novoDiv.innerHTML = `
+	novoDiv.innerHTML = `
             <div class="imagem-container">
                 <a href="" class="icones fechar">&#128473;</a>
                 <a href="#${iDAnterior}" class="icones anterior">&#129168;</a>
                 <img src="${urlImagem}" alt="">
                 <a href="#${iDPosterior}" class="icones proximo">&#129170;</a>
-            </div>`
+            </div>`;
 
-    container.appendChild(novoDiv)
-}
+	container.appendChild(novoDiv);
+};
 
-const carregarSlide = (imagens) => imagens.forEach(criarSlide)
+const carregarSlide = (imagens) => imagens.forEach(criarSlide);
 
-const carregarImagens = (imagens) => imagens.forEach(criarItem)
+const carregarImagens = (imagens) => imagens.forEach(criarItem);
 
-carregarImagens(imagens.url)
-carregarSlide(imagens.url)
+carregarImagens(imagens.url);
+carregarSlide(imagens.url);
